4b4aec7af53cd67b77fa3b1746598bd0715ddbc7
João Vitor Ferreira <jvferreira.20@gmail.com>
2023-10-28 22:11:34 -0300

    Exercise 2 solved

--- testes/modelo.test.js
+++ testes/modelo.test.js
@@ -16,10 +16,37 @@ test('Testando cadastro de três perguntas', () => {
   modelo.cadastrar_pergunta('1 + 1 = ?');
   modelo.cadastrar_pergunta('2 + 2 = ?');
   modelo.cadastrar_pergunta('3 + 3 = ?');
-  const perguntas = modelo.listar_perguntas(); 
+  const perguntas = modelo.listar_perguntas();
   expect(perguntas.length).toBe(3);
   expect(perguntas[0].texto).toBe('1 + 1 = ?');
   expect(perguntas[1].texto).toBe('2 + 2 = ?');
   expect(perguntas[2].num_respostas).toBe(0);
-  expect(perguntas[1].id_pergunta).toBe(perguntas[2].id_pergunta-1);
+  expect(perguntas[1].id_pergunta).toBe(perguntas[2].id_pergunta - 1);
+});
+
+test('Testando cadastro de uma resposta', () => {
+  modelo.cadastrar_pergunta('1 + 1 = ?');
+  const id_pergunta = modelo.listar_perguntas()[0].id_pergunta;
+  modelo.cadastrar_resposta(id_pergunta, '2');
+  const respostas = modelo.get_respostas(id_pergunta);
+  expect(respostas.length).toBe(1);
+  expect(respostas[0].texto).toBe('2');
+  expect(respostas[0].id_pergunta).toBe(id_pergunta);
+  expect(respostas[0].id_resposta).toBeGreaterThan(0);
+});
+
+
+test('Testando busca de pergunta por ID', () => {
+  const id_pergunta = modelo.cadastrar_pergunta('5 x 5');
+  const pergunta = modelo.get_pergunta(id_pergunta);
+  expect(pergunta.texto).toBe('5 x 5');
+});
+
+test('Testando busca de número de respostas', () => {
+  const id_pergunta = modelo.cadastrar_pergunta('Qual a cor do céu?');
+  modelo.cadastrar_resposta(id_pergunta, 'Vermelho');
+  modelo.cadastrar_resposta(id_pergunta, 'Azul');
+  modelo.cadastrar_resposta(id_pergunta, 'Verde');
+  const numRespostas = modelo.get_num_respostas(id_pergunta);
+  expect(numRespostas).toBe(3);
 });
